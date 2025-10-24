7b83db9364ada5950ec07656328a66412e8e2209
44YHC <contact.yurii.vlasiuk@gmail.com>
2025-10-24 12:43:56 +0300

    clickhouse: s3 storage, config

--- run-portainer-local.sh
+++ run-portainer-local.sh
@@ -1,4 +1,6 @@
 docker run -d -p 8000:8000 -p 9443:9443 \
-  --name portainer --restart=unless-stopped \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v `pwd`/_portainer_data:/data portainer/portainer-ce:latest
+    --name portainer \
+    --restart=unless-stopped \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
