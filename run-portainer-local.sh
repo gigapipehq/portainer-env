3ebbefdd1653a78ba0b5138cd4aab4efc78698bc
akvlad <akvlad90@gmail.com>
2025-10-24 13:38:36 +0300

    use --network host for portainer app

--- run-portainer-local.sh
+++ run-portainer-local.sh
@@ -1,5 +1,6 @@
 docker run -d -p 8000:8000 -p 9443:9443 \
     --name portainer \
+    --network host \
     --restart=unless-stopped \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
